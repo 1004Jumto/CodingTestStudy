e83ff49062587ff19f413905f0a3a77d97aa16a4
Yejin <leeyjin0926@gmail.com>
2026-02-09 12:38:49 +0900

    [PGS] H-index

--- week05/예진/[PGS] 최소직사각형.java
+++ week05/예진/[PGS] 최소직사각형.java
@@ -3,17 +3,17 @@ class Solution {
         int answer = 0;
         int maxW = 0;
         int maxH = 0;
-        
-        for(int[] size : sizes) {
+
+        for (int[] size : sizes) {
             int w = Math.max(size[0], size[1]);
             int h = size[0] == w ? size[1] : size[0];
-            
+
             maxW = Math.max(maxW, w);
             maxH = Math.max(maxH, h);
         }
-        
+
         answer = maxW * maxH;
-        
+
         return answer;
     }
 }
